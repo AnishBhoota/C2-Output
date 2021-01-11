1080a93176ec63fef2456cedad1432ecddf00ffb
Bhoota Anish <110155@eesd.org>
2021-01-11 23:56:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print ("My name is Anish")
+print("My favorite color is black")
+print ("My favorite sports are badminton")
+print("I am in 6th grade")
+print("My favorite ice cream flavor is cookies and cream")
+#My Info
+
